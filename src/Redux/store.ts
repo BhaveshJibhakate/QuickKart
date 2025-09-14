0ae895e32b92c94ff98d794762e7bc85867e70ee
Bhavesh <bhavesh.jibhakate@gmail.com>
2025-09-14 21:40:26 +0530

    did force npm install

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,22 +1,22 @@
-import { createStore,combineReducers,applyMiddleware } from "redux";
-import proudctReducer from "./productReducer";
-import cartReducer from "./cartReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { watchProductSaga } from "./Saga/productSaga";
-import createSagaMiddleware from "redux-saga";
+    import { createStore,combineReducers,applyMiddleware } from "redux";
+    import proudctReducer from "./productReducer";
+    import cartReducer from "./cartReducer";
+    import { composeWithDevTools } from "redux-devtools-extension";
+    import { watchProductSaga } from "./Saga/productSaga";
+    import createSagaMiddleware from "redux-saga";
 
 
-const rootReducer=combineReducers({
-    products:proudctReducer,
-    cart:cartReducer
-})
-const sagaMiddleware=createSagaMiddleware()
+    const rootReducer=combineReducers({
+        products:proudctReducer,
+        cart:cartReducer
+    })
+    const sagaMiddleware=createSagaMiddleware()
 
-const store=createStore(rootReducer,undefined,composeWithDevTools(applyMiddleware(sagaMiddleware)))
+    const store=createStore(rootReducer,undefined,composeWithDevTools(applyMiddleware(sagaMiddleware)))
 
-sagaMiddleware.run(watchProductSaga)  // sagamiddleware should be run after mounting on store
+    sagaMiddleware.run(watchProductSaga)  // sagamiddleware should be run after mounting on store
 
 
 
-export default store
+    export default store
 
